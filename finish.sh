4fd03600d16f1d1ea5e7c32049d9b1aedc87d11c
thethundah5 <francoreyes16@gmail.com>
2023-06-04 14:48:01 -0400

    ajout regle, AZC-HUB-PROD, SC-15432

--- finish.sh
+++ finish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git add .
-current= git branch --show-current
-echo $current
-echo "ajouter un commentaire au billet $current"
+current=$(git branch --show-current &2>/dev/null)
+mess1="ajouter un commentaire au billet"
+echo $mess1 $current
 read commentaire
 echo "vous avez ajouter un commentaire pour" $current
 git commit -m "$commentaire"
